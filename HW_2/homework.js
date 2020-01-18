01ad69015be8d2252186d5537fee9b042f51538d
Valentyn <v.kormilets@gmail.com>
2020-01-19 00:38:02 +0200

    Add HW2

--- HW_2/homework.js
+++ HW_2/homework.js
@@ -1,7 +1,45 @@
+function age(){
+    let age = prompt('Сколько Вам лет исполняется или уже исполнилось в этом году: ');
+    const date = new Date();
+    alert(`Год Вашего рождения: ${date.getFullYear() - age}`);
+}
+
+function convert(degree) {
+    var x;
+    if (degree == "cels") {
+        x = document.getElementById("celsium").value * 9 / 5 + 32;
+        document.getElementById("farengeit").value = Math.round(x);
+    } else {
+        x = (document.getElementById("farengeit").value -32) * 5 / 9;
+        document.getElementById("celsium").value = Math.round(x);
+    }
+}
+function floor() {
+    let x = -43.555555, y = 3.3333333;
+    if(x/y>=0)
+        alert(Math.floor(x/y));
+    else
+        alert(Math.ceil(x/y));
+}
+
+
+function badWords (){
+    let text = prompt('Введите текст (не вздумай вводить слово bad)');
+    if (text.indexOf("bad") !== -1){
+        alert ('There is bad words')
+    } else alert ('OK')
+}
+
+
+function hello (){
+    let name = prompt ("Ваше имя:");
+    alert (`Привет, ${name}`);
+}
+
 function gender() {
     confirm('Если ты мужчина - нажми ОК!') ? alert ('Ты мужик!') : alert ('Вы женщина')
 }
-gender();
+
 
 function calcFloor(){
     var floorNum = 5;
@@ -12,7 +50,7 @@ function calcFloor(){
     let ourFloor = Math.ceil((ourFlat - (floorNum * countFlat * except)) / countFlat);
     alert(`Ваш подьезд: ${padik}, Ваш этаж: ${ourFloor}`);
 }
-calcFloor();
+
 
 
 function numberOdd() {
@@ -22,4 +60,59 @@ function numberOdd() {
         numberOdd();
     } else alert((ourNum % 2 === 0 ) ? 'Число четное' : 'Число нечетное');
 }
-numberOdd();
+
+var arr = [{
+    name: "Vasya",
+    surname: "Petrov"
+},{
+    name: "Ivan",
+    surname: "Pupkin"
+}
+];
+console.log(arr);
+arr.push('234');
+arr[0].name = "Sigizmund";
+
+
+
+function kNb(){
+        var myHod = prompt('Введите Ваш вариант: Камень, Ножницы, Бумага', "");
+        var compHod = Math.random();
+        console.log (compHod);
+        if (compHod < 0.3333){
+            compHod = 'Камень';
+            if (myHod === 'Камень'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. НИЧЬЯ!`)
+            }
+            if (myHod === 'Ножницы'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. Вы проиграли!`)
+            }
+            if (myHod === 'Бумага'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. Вы выиграли!`)
+            }
+        }
+        if (compHod >= 0.3333 && compHod < 0.6666){
+            compHod = 'Ножницы';
+            if (myHod === 'Камень'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. Вы выиграли!`)
+            }
+            if (myHod === 'Ножницы'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. НИЧЬЯ!`)
+            }
+            if (myHod === 'Бумага'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. Вы проиграли!`)
+            }
+        }
+        if (compHod >= 0.6666){
+            compHod = 'Бумага';
+            if (myHod === 'Камень'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. Вы проиграли!`)
+            }
+            if (myHod === 'Ножницы'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. Вы выиграли!`)
+            }
+            if (myHod === 'Бумага'){
+                alert(`У Вас ${myHod}, у оппонента ${compHod}. НИЧЬЯ!`)
+            }
+        }
+}
